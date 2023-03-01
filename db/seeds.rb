28236b63914e8a0c82e2d09523e91cdb055e4506
samuelmoiwa <samuelmoiwa@gmail.com>
2023-03-01 23:34:05 +0000

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Greetings.create([
+Greeting.create([
   { message: "Hello" },
   { message: "Hi" },
   { message: "Hey there" },
